fdb91fa1a83dab9476fb51bc377cd99af010e008
HesaraLK <gurue052@gmail.com>
2025-06-10 19:15:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,19 @@
 const fs = require("fs");
-if (fs.existsSync("config.env"))
+if (fs.existsSync("config.env")) {
   require("dotenv").config({ path: "./config.env" });
+}
 
 function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
+
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "78pFDZLT#LHxaIp6lXPyCaRbZsbECnPISKDVKgfMJq0r4GT_l3aY",
   MONGODB: process.env.MONGODB || "mongodb://mongo:kdBNfZeyTtBWRKidlYBpelicPZcBMkbn@switchyard.proxy.rlwy.net:12723",
   OWNER_NUM: process.env.OWNER_NUM || "94773207500",
+
+  // 🔧 NEW CONFIGS ADDED (used by index.js)
+  PREFIX: process.env.PREFIX || ".",
+  MODE: process.env.MODE || "public", // options: public | private | groups | inbox
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
